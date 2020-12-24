dcfeefe74da904acf4f292dd586d6032ae13ebcf
reona-fujii <ya5t4yfxwbcshb908r0s@gmail.com>
2020-12-24 16:19:50 +0900

    first_commit

---
+++ src/if01.java
@@ -0,0 +1,33 @@
+public class if01 {
+	public static void main(String[] args) {
+//	int distance = 7;
+//	if (distance <= 5) {
+//		System.out.println("近い");
+//	} else if (distance <= 10){
+//		System.out.println("そこそこ");
+//	} else if (distance > 10) {
+//		System.out.println("遠い");
+//	}
+//	}
+
+//	String color = "red";
+//	switch (color) {
+//	case "red":
+//		System.out.println("赤");
+//		break;
+//	case "yellow":
+//		System.out.println("黄");
+//		break;
+//	case "blue" :
+//		System.out.println("青");
+//		break;
+//	default:
+//		System.out.println("なし");
+//	}
+	
+	
+		
+	}
+	
+		
+}
